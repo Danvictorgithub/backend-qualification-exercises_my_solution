8376b6be380ba1942f155dae028c311cba7acef6
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-02-26 22:09:41 +0800

    finished level1

--- src/level1/problem2/index.ts
+++ src/level1/problem2/index.ts
@@ -1,16 +1,29 @@
+import crypto from 'crypto';
 export class ObjectId {
   private data: Buffer;
-
+  private static fourBitsRandom = crypto.randomBytes(4);
+  private static counter = Math.floor(Math.random() * (Math.pow(2, 24)));
   constructor(type: number, timestamp: number) {
-    /**
-     * insert your code here
-     */
+    // This solution doesn't work
+    // let fourBitsRandom = crypto.randomBytes(4);
+    // let counter = Math.floor(Math.random() * (Math.pow(2, 24)));
+
+    // this.data.set(fourBitsRandom, 7);
+    // this.data.writeUIntBE(counter + 1, 11, 3);
+    // counter = (counter + 1) % Math.pow(2, 24);
+
+    this.data = Buffer.alloc(14); // 1 + 6 + 4 + 3
+    this.data.writeUInt8(type, 0);
+    this.data.writeUIntBE(timestamp, 1, 6);
+    this.data.set(ObjectId.fourBitsRandom, 7);
+    this.data.writeUIntBE(ObjectId.counter + 1, 11, 3);
+    ObjectId.counter = (ObjectId.counter + 1) % Math.pow(2, 24);
   }
 
   static generate(type?: number): ObjectId {
     return new ObjectId(type ?? 0, Date.now());
   }
-  
+
   toString(encoding?: 'hex' | 'base64'): string {
     return this.data.toString(encoding ?? 'hex');
   }
